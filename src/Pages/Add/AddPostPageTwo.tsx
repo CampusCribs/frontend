33eb8ed139a284eb3baf44c6484583f208ccf11c
Chris Farah <farahca@mail.uc.edu>
2025-02-19 21:07:29 -0500

    image upload

--- src/Pages/Add/AddPostPageTwo.tsx
+++ src/Pages/Add/AddPostPageTwo.tsx
@@ -1,5 +1,5 @@
 import { Label } from "@radix-ui/react-label";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CalendarComponent from "./CalendarComponent";
 import { Input } from "@/components/ui/input";
 
@@ -9,6 +9,13 @@ type Props = {
 };
 
 const AddPostPageTwo = ({ hide, submit }: Props) => {
+  const [images, setImages] = useState<File[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setImages([...images, ...Array.from(e.target.files)]);
+    }
+  };
   useEffect(() => {
     if (submit) {
       console.log("page two submit");
@@ -33,7 +40,21 @@ const AddPostPageTwo = ({ hide, submit }: Props) => {
           <Label htmlFor="title" className="">
             Upload images:
           </Label>
-          <Input type="file" />
+          <Input type="file" accept="image/*" onChange={handleChange} />
+        </div>
+        <div className="p-3 flex flex-row flex-wrap w-full items-center justify-left">
+          {images &&
+            Array.from(images).map((image) => (
+              <img
+                key={image.name}
+                src={URL.createObjectURL(image)}
+                alt="uploaded image"
+                onClick={() => {
+                  setImages(images.filter((i) => i.name !== image.name));
+                }}
+                className="w-32 h-32 object-cover m-2 border border-black rounded-xl shadow-xl "
+              />
+            ))}
         </div>
       </div>
     </div>
